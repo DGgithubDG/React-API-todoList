ce60d2afb8abd3e9cc5e40c34e575932653cc2f9
DGgithubDG <thenumerousrandom@gmail.com>
2022-11-11 18:30:23 +0000

    React-todo-with-fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
@@ -6,8 +6,43 @@ export function Home() {
 	const [tasklist, setTaskList] = useState([]);
 	const [task, setTask] = useState("");
 	const [hoverTask, setHoverTask] = useState(false);
-
+// 	useEffect(() => {
+// sum(num1,num2)
+// 	}
+// 	,[]);
+	useEffect(() => {
 	
+	fetch('https://assets.breatheco.de/apis/fake/todos/user/denis', {
+		method: "GET",
+		//body: JSON.stringify(tasklist),
+		headers: {
+		  "Content-Type": "application/json"
+		}
+	  })
+	  .then(resp => {
+		  console.log(resp.ok); // will be true if the response is successfull
+		  console.log(resp.status); // the status code = 200 or code = 400 etc.
+		 // console.log(resp.text()); // will try return the exact result as string
+		  return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+	  })
+	  .then(data => {
+		  //here is were your code should start after the fetch finishes
+		  console.log(data); //this will print on the console the exact object received from the server
+	  })
+	  .catch(error => {
+		  //error handling
+		  console.log(error);
+	  });
+	},[]);
+	//useEffect(() => {
+		//fetch("http://assets.breatheco.de/apis/fake/todos/denis"
+	//.then(res => {
+		//return package.json();
+	//})
+	//.then(tasklist => {
+	//	setTask(tasklist);
+	//});
+//}, []);
 	const handleOnKeyPress = e => {
 		
 		if (e.key === "Enter" && task !== "") {
